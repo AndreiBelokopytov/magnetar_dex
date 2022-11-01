4dc3229610c151bc963cea19420e6f388fed830f
Andrei Belokopytov <belokopytov87@gmail.com>
2022-11-01 18:45:30 +0300

    Fix build process

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,12 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "public/synths", to: "synths" }],
+    }),
+  ],
 });
